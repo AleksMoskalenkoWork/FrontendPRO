78d68864ced22c1d0d32ed123c097d5508602c0d
AleksMoskalenko <aleks.moskalenko.work@gmail.com>
2025-06-17 11:04:57 +0300

    clean up

---
+++ homeWorks/homeWork-29/counter/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloremque
+        expedita dignissimos, sint molestias maxime unde tempore nihil debitis
+        deleniti, natus laborum dolores eum quis temporibus ratione quibusdam
+        cum aut id.
+      </p>
+    </>
+  );
+}
+
+export default App;
