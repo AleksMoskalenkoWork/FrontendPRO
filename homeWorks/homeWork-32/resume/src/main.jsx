07a5cf65fde08f52fee09279ea0e41603262b145
AleksMoskalenko <aleks.moskalenko.work@gmail.com>
2025-06-27 10:55:25 +0300

    create resume project
    clean up & add redux & roting

---
+++ homeWorks/homeWork-32/resume/src/main.jsx
@@ -0,0 +1,23 @@
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    children: [
+      { index: true, element: <HomePage /> },
+      { path: 'todo', element: <TodoPage /> },
+      { path: 'swapi', element: <SwapiPage /> },
+    ],
+  },
+]);
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>
+);
