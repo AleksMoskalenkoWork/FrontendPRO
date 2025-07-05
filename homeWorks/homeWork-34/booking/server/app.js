cfe3e37ad742b0e51394919afac77854b35df060
AleksMoskalenko <aleks.moskalenko.work@gmail.com>
2025-07-05 21:50:03 +0300

    add parser for json

--- homeWorks/homeWork-34/booking/server/app.js
+++ homeWorks/homeWork-34/booking/server/app.js
@@ -1,14 +1,31 @@
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv').config();
-const dbConnection = require('./DBConnection');
+const { dbConnection, runDataBase } = require('./DBConnection');
+const path = require('path');
+const fs = require('fs');
 
 const app = express();
-
-dbConnection();
+runDataBase();
 
 app.use(cors());
 app.use(express.json());
+
+app.get('/parsJson', async (req, res) => {
+  try {
+    const db = await dbConnection();
+    const dataPath = path.resolve(__dirname, 'db.json');
+    const data = fs.readFileSync(dataPath, 'utf-8');
+    const json = JSON.parse(data).destination;
+
+    const result = await db.collection('destination').insertMany(json);
+
+    res.send(`${result.insertedCount} documents inserted successfully`);
+  } catch (error) {
+    console.error('Insert error:', error);
+    res.status(500).send('Internal Server Error');
+  }
+});
 
 app.listen(process.env.PORT, () => {
   console.log(
